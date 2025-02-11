a964881fab8b2703a19ce63eb9788709c07b2bd8
bovillaios <48376360+bovillaios@users.noreply.github.com>
2025-02-11 15:23:09 +0530

    Update MyLibrary.swift

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct MyLibrary {
+    public func sumOfTwoInts(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
